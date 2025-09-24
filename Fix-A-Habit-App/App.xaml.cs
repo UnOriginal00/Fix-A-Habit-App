5203bb9b1ec2ea949bae101db65e00b7c97b1771
Ripfumelo Junior Chauke <pfumichauke4@gmail.com>
2025-09-24 19:16:46 +0200

    Initial commit

---
+++ Fix-A-Habit-App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Fix_A_Habit_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Fix-A-Habit-App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Fix_A_Habit_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
